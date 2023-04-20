c27b22946a36883fbc22fdc4a196e6bc04e66fae
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-20 20:48:47 +0200

    añadir tarea funcion
    en proceso de mejora

--- funciones.js
+++ funciones.js
@@ -1,5 +1,19 @@
 
+
+/* añadir tarea */
+
+
 function añadirTarea() {
+let li = document.getElementsByTagName('li');
+
+for (let i = 0; i < li.length; i++) {
+    let button = document.createElement('span');
+    let text = document.createTextNode("\u00D7");
+
+    button.className = 'close';
+    button.appendChild(text);
+
+    }
+}
 
 
-} 
