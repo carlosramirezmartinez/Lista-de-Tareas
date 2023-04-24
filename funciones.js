eed1ebfea4c1e3eea382fe41838bfa7c3e83df55
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-24 22:27:33 +0200

    Update funciones.js
    salida undefined boton añadir close ok

--- funciones.js
+++ funciones.js
@@ -4,37 +4,36 @@
 
 
 function añadirTarea() {
-let li = document.getElementsByTagName('li');
-let inputTarea = document.getElementById('inputTarea').value;
-//console.log(li);
+    let li = document.createElement('li');
+    let inputTarea = document.getElementById('inputTarea').value;
+    let inputText = document.createTextNode(inputTarea)
+    li.appendChild(inputText);
+
+    let ul = document.getElementById('ListaPendiente')
 
 
-let inputText = document.createTextNode(inputTarea)
-li.appendChild(inputText);
-
-let UL = document.getElementById('ListaPendiente')
+    inputTarea === "" ? alert('Escribe una tarea para añadir') : ul.appendChild(li)
 
 
-inputTarea === "" ? alert('Escribe una tarea para añadir') : UL.appendChild(li)
+   
+    let button = document.createElement('span');
 
-let button = document.createElement('span');
-
-let text = document.createTextNode("\u00D7");
-
-
-button.className = 'close';
-
-button.appendChild(text)
-
-li.appendChild(button);
+    let text = document.createTextNode(`\u00D7`);
 
 
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none"
+    button.className = 'close';
 
+    button.appendChild(text)
+
+    li.appendChild(button);
+
+
+    let close = document.getElementsByClassName('close');
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none"
+        }
     }
-
-    }
-}
+   
+} 
