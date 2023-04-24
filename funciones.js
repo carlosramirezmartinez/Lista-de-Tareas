41d028b6ea1f6bef81f8f0b532326e5498c0a082
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-24 21:56:24 +0200

    Update funciones.js
    Falta por mejorar, no funciona

--- funciones.js
+++ funciones.js
@@ -5,15 +5,36 @@
 
 function añadirTarea() {
 let li = document.getElementsByTagName('li');
+let inputTarea = document.getElementById('inputTarea').value;
+//console.log(li);
 
-for (let i = 0; i < li.length; i++) {
-    let button = document.createElement('span');
-    let text = document.createTextNode("\u00D7");
 
-    button.className = 'close';
-    button.appendChild(text);
+let inputText = document.createTextNode(inputTarea)
+li.appendChild(inputText);
+
+let UL = document.getElementById('ListaPendiente')
+
+
+inputTarea === "" ? alert('Escribe una tarea para añadir') : UL.appendChild(li)
+
+let button = document.createElement('span');
+
+let text = document.createTextNode("\u00D7");
+
+
+button.className = 'close';
+
+button.appendChild(text)
+
+li.appendChild(button);
+
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none"
+
+    }
 
     }
 }
-
-
